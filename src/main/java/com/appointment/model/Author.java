ec9483cdef4e2376e1e2b0fcf6c8b7e5e7adfdb8
rakesh <rksinghyadav1995@gmail.com>
2019-05-26 22:39:04 +0530

    author and book class entity added

--- src/main/java/com/appointment/model/Author.java
+++ src/main/java/com/appointment/model/Author.java
@@ -1,36 +1,50 @@
 package com.appointment.model;
 
-public class Author {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
 
-	//this is id
+@Entity
+@Table
+public class Author {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private int id;
 	private String name;
 	private String qualification;
+	@OneToOne(mappedBy = "author")
+	private Book book;
+
 	public int getId() {
 		return id;
 	}
+
 	public void setId(int id) {
 		this.id = id;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public String getQualification() {
 		return qualification;
 	}
+
 	public void setQualification(String qualification) {
 		this.qualification = qualification;
 	}
-	
-	
-	
+
 	@Override
 	public String toString() {
 		return "Author [id=" + id + ", name=" + name + ", qualification=" + qualification + "]";
 	}
-	
-	
+
 }
---
+++ src/main/java/com/appointment/model/Book.java
@@ -0,0 +1,62 @@
+package com.appointment.model;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table
+public class Book {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
+	private String bookName;
+	private String titile;
+	@OneToOne(cascade = CascadeType.ALL)
+	@JoinColumn(name = "id", referencedColumnName = "id")
+	private Author author;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getBookName() {
+		return bookName;
+	}
+
+	public void setBookName(String bookName) {
+		this.bookName = bookName;
+	}
+
+	public String getTitile() {
+		return titile;
+	}
+
+	public void setTitile(String titile) {
+		this.titile = titile;
+	}
+
+	public Author getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(Author author) {
+		this.author = author;
+	}
+
+	@Override
+	public String toString() {
+		return "Book [id=" + id + ", bookName=" + bookName + ", titile=" + titile + ", author=" + author + "]";
+	}
+
+}
